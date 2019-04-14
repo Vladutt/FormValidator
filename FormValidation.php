7cd329d2e12157a2c95c4be511513beff60c867b
Vlăduț Teodor <vladutteodor18@gmail.com>
2019-04-14 16:08:15 +0300

    Update FormValidation.php

--- FormValidation.php
+++ FormValidation.php
@@ -6,28 +6,28 @@ class FormValidation {
   protected $customErrors = [];
   protected $rules = [];
 
-  public function __construct(array $data){
+  public function __construct(array $data) {
     $this->data = $data;
   }
 
-  public function setRules(array $rules){
+  public function setRules(array $rules) {
     $this->rules = $rules;
   }
 
-  public function isValidated(){
+  public function isValidated() {
 
     $this->process();
     return count($this->errors) ? 0 : 1;
 
   }
 
-  protected function process(){
+  protected function process() {
 
-    foreach($this->rules as $field => $rule){
+    foreach($this->rules as $field => $rule) {
 
-      if(isset($this->data[$field])){
+      if(isset($this->data[$field])) {
         
-        if(strpos($rule, '|') == true){
+        if(strpos($rule, '|') == true) {
 
           $bits = explode('|', $rule);
           foreach($bits as $bit){
@@ -46,8 +46,8 @@ class FormValidation {
 
   }
 
-  protected function processSecondRule($rule, $field){
-    if(strpos($rule, ':') == true){
+  protected function processSecondRule($rule, $field) {
+    if (strpos($rule, ':') == true) {
       $pieces = explode(':', $rule);
       $this->{$pieces[0]}($field, $pieces[1]);
     }else{
@@ -55,7 +55,7 @@ class FormValidation {
     }
   }
 
-  public function setCustomErrors(array $errors){
+  public function setCustomErrors(array $errors) {
     foreach($errors as $field => $error){
       if(strpos($field, '.') == true){
         $this->customErrors[$field] = $error;
@@ -65,10 +65,10 @@ class FormValidation {
     }
   }
 
-  protected function required($field){
-    if(empty($this->data[$field])){
+  protected function required($field) {
+    if (empty($this->data[$field])) {
       $message = 'This field ' . $field . ' is empty.';
-      if(empty($this->customErrors[$field.__FUNCTION__])){
+      if (empty($this->customErrors[$field.__FUNCTION__])) {
         $this->newErrors($field, $message); 
       }else{
         $this->newErrors($field, $this->customErrors[$field.__FUNCTION__]);
@@ -76,10 +76,10 @@ class FormValidation {
     }
   }
 
-  protected function email($field){
-    if(!filter_var($this->data[$field], FILTER_VALIDATE_EMAIL)){
+  protected function email($field) {
+    if (!filter_var($this->data[$field], FILTER_VALIDATE_EMAIL)) {
       $message = "This field ' . $field . ' doesn't have an valid email.";
-      if(empty($this->customErrors[$field.__FUNCTION__])){
+      if (empty($this->customErrors[$field.__FUNCTION__])) {
         $this->newErrors($field, $message); 
       }else{
         $this->newErrors($field, $this->customErrors[$field.__FUNCTION__]);
@@ -88,9 +88,9 @@ class FormValidation {
   }
 
   protected function min($field, $length){
-    if(strlen($this->data[$field]) < $length){
+    if (strlen($this->data[$field]) < $length) {
       $message = 'This field ' . $field . ' needs to have more than ' . $length . ' characters.';
-      if(empty($this->customErrors[$field.__FUNCTION__])){
+      if (empty($this->customErrors[$field.__FUNCTION__])) {
         $this->newErrors($field, $message); 
       }else{
         $this->newErrors($field, $this->customErrors[$field.__FUNCTION__]);
@@ -98,10 +98,10 @@ class FormValidation {
     }
   }
 
-  protected function max($field, $length){
-    if(strlen($this->data[$field]) > $length){
+  protected function max($field, $length) {
+    if (strlen($this->data[$field]) > $length) {
       $message = 'This field ' . $field . ' needs to have max ' . $length . ' characters.';
-      if(empty($this->customErrors[$field.__FUNCTION__])){
+      if (empty($this->customErrors[$field.__FUNCTION__])) {
         $this->newErrors($field, $message); 
       }else{
         $this->newErrors($field, $this->customErrors[$field.__FUNCTION__]);
@@ -109,12 +109,12 @@ class FormValidation {
     }
   }
 
-  public function getErrors(){
+  public function getErrors() {
     return $this->errors;
   }
-  public function newErrors($field, $message){
+  public function newErrors($field, $message) {
 
-    if(!isset($this->errors[$field])){
+    if (!isset($this->errors[$field])) {
       $this->errors[$field] = $message;
     }
 
